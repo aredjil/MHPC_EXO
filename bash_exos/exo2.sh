05e1cf6af6ada7733dd6e3195ea12eaf50c69af3
aredjil <random@gmail.com>
2024-09-11 15:24:23 +0200

    fixed exo2, now it works

--- bash_exos/exo2.sh
+++ bash_exos/exo2.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-# The aim of this script is to return a specified oepration between two given numbers
+## The aim of this script is to return a specified oepration between two given numbers
 options=("sum" "sub" "division" "multiplication" "Quit")
 add(){
-read -p "Please type the first number " n1
-read -p "Please type the second number " n2
-echo "The sum is: $(($n1+$n2))"
+read -p "Type the first number " n1
+read -p "Type the second number " n2
+echo "n1 + n2= $(($n1+$n2))"
 }
 sub(){
-read -p "Please type the first number " n1
-read -p "Please type the second number " n2
-echo "The substraction is: $(($n1-$n2))"
+read -p "Type the first number " n1
+read -p "Type the second number " n2
+echo "n1 -n2= $(($n1-$n2))"
 }
 mult(){
-read -p "Please type the first number " n1
-read -p "Please type the second number " n2
+read -p "Type the first number " n1
+read -p "Type the second number " n2
 echo `expr $n1 \* $n2`
 }
 div(){
-read -p "Please type the first number " n1
-read -p "Please type the second number " n2
-echo `expr $n1/$n2` 
+read -p "Type the first number " n1
+read -p "Type the second number " n2
+echo "n1/n2= $(($n1/$n2))"
 }
 
 PS3="Please enter your choice: "
