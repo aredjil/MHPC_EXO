c5e5e6ae0708a61b3b5e1847a78eb31f16d59367
aredjil <random@gmail.com>
2024-09-12 16:41:56 +0200

    added exo4

--- bash_exos/exo4.sh
+++ bash_exos/exo4.sh
@@ -1,2 +1,16 @@
 #! /bin/bash 
-echo "This is an echo from inside the text"
+path="/home/aredjil/Documents/MHPC_2024_2025/MHPC_EXO/bash_exos/kernal"
+for file in "$path"/*
+do
+if [[ "$file" == *.sh ]]
+then
+       if [[ ! -x $file ]] 
+       then
+	       chmod +x $file
+       else  
+	       chmod -x $file
+	fi 	
+else
+	echo "There are no files ending with .sh in this directory"
+fi
+done 
